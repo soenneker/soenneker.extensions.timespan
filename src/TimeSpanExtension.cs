ad5fffbb90c6eacb53c60e8cf35a861de767ef60
Jake Soenneker <jake@soenneker.com>
2025-03-29 09:48:22 -0500

    Addt method

--- src/TimeSpanExtension.cs
+++ src/TimeSpanExtension.cs
@@ -158,4 +158,15 @@ public static class TimeSpanExtension
     {
         return timeSpan.ToString(@"hh\:mm\:ss");
     }
+
+    /// <summary>
+    /// Converts a time span into a string formatted as hours, minutes, and seconds.
+    /// </summary>
+    /// <param name="timeSpan">Represents a duration of time that will be formatted into a string.</param>
+    /// <returns>A string representation of the time span in the format 'hh:mm:ss'.</returns>
+    [Pure]
+    public static string ToMinuteSecondString(this System.TimeSpan timeSpan)
+    {
+        return timeSpan.ToString(@"mm\:ss");
+    }
 }
